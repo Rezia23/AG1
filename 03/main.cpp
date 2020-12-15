89cd1f211795632e1e671b554e6e4f7a81fe37c8
Terezie Hrubanova <terezie-h@seznam.cz>
2020-12-15 22:29:47 +0100

    start recursive func

--- 03/main.cpp
+++ 03/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <memory>
+#include <set>
 using namespace std;
 
-using Week = int;
+using WeekNum = int;
 
 class Measure{
 public:
@@ -17,14 +19,38 @@ public:
 };
 
 class Election{
-    int minPopularity;
 public:
+    int requiredPopularity;
+
     Election(int popularity) {
-        minPopularity = popularity;
+        requiredPopularity = popularity;
     }
     void pretty_print(){
-        cout << "min popularity: " << minPopularity;
+        cout << "min popularity: " << requiredPopularity;
     }
+};
+
+enum ActionType{ZAVEST, ZRUSIT};
+
+class Action{
+public:
+    int actionNum;
+    ActionType type;
+};
+
+
+class Week{
+public:
+    WeekNum week;
+    vector<Action> actions{};
+    int numInfected;
+    int popularity;
+    Week (WeekNum week, int numInfected, int popularity){
+        this->week = week;
+        this->numInfected = numInfected;
+        this->popularity = popularity;
+    }
+    //todo void addAction()
 };
 
 
@@ -39,7 +65,7 @@ public:
     int numInfected;
     double reproduction;
     vector<Measure> measures;
-    map<Week, Election> elections;
+    map<WeekNum, Election> elections;
     void pretty_print(){
         cout << "Num weeks " <<weeks << "\nNum max active " << maxNumActiveMeasures << "\nnum measures "
         << numMeasures << "\nnumElections " << numElections << "\nstart popularity " << startPopularity
@@ -55,7 +81,32 @@ public:
         }
         cout << endl;
     }
+    
+    shared_ptr<Week> finalWeek;
 };
+
+
+//this is where all the magic happens (eventually)
+bool findSolutions(const Week & prevWeek, int currInfected, int currPopularity,
+        int currNumMeasures, set<int> & canceledMeasures, SetUp & setup){
+    WeekNum currWeekNum = prevWeek.week + 1;
+
+
+    //should be at the end, if input is not already calculated for this week (as currPopularity)
+    if(currNumMeasures > setup.maxNumActiveMeasures){
+        return false;
+    }
+    if(currPopularity < 0){
+        return false;
+    }
+    if(setup.elections.find(currWeekNum) != setup.elections.end()){
+      if(currPopularity < setup.elections.find(currWeekNum)->second.requiredPopularity){
+          return false;
+      }
+    }
+    //todo
+}
+
 
 
 int main() {
@@ -79,6 +130,10 @@ int main() {
         setup.pretty_print();
 
         //calculate all the stuff
+        Week firstWeek = Week()
+        set<int> canceledMeasures = {};
+
+        findSolutions(beginningAction,setup.numInfected, setup.startPopularity, 0, canceledMeasures, setup)
 
     }
 
